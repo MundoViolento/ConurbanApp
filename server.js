5bf0540dd9059875a7a8fd539a1d4ec9ee91a7e3
Tomi-Godoy0 <tomasgodoy037@gmail.com>
2022-12-05 17:41:25 -0300

    Hice funcionar el inicio de sesion y agregue mensajes

--- server.js
+++ server.js
@@ -38,8 +38,10 @@ app.use(flash())
 
 // Variables globales
 app.use((req, res, next) => {
-    res.locals.succes_msg = req.flash('succes_msg')
+    res.locals.success_msg = req.flash('success_msg')
     res.locals.error_msg = req.flash('error_msg')
+    res.locals.error = req.flash('error')
+    res.locals.user = req.user || null
     next()
 })
 
